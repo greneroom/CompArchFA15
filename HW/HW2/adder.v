ebaa0b9aaae1066955192f64d02c4da8d18ee395
David Abrahams <david.abrahams@students.olin.edu>
2015-09-22 15:50:54 -0400

    wrote adder. untested.

--- HW/HW2/adder.v
+++ HW/HW2/adder.v
@@ -5,9 +5,20 @@ assign {carryout, sum}=a+b+carryin;
 endmodule
 
 module structuralFullAdder(out, carryout, a, b, carryin);
-output out, carryout;
-input a, b, carryin;
-  // Your adder code here
+
+  output out, carryout;
+  input a, b, carryin;
+
+  wire a_xor_b;
+  wire a_and_b;
+  wire a_xor_b_and_cin;
+
+  xor xor_gate_0(a_xor_b, a, b);
+  xor xor_gate_1(sum, a_xor_b, carryin);
+  and and_gate_0(a_and_b, a, b);
+  and and_gate_1(a_xor_b_and_cin, a_xor_b, carryin);
+  or or_gate(carryout, a_and_b, a_xor_b_and_cin);
+
 endmodule
 
 module testFullAdder;
@@ -15,7 +26,7 @@ reg a, b, carryin;
 wire sum, carryout;
 behavioralFullAdder adder (sum, carryout, a, b, carryin);
 
+// TODO
 initial begin
-  // Your test code here
 end
 endmodule
