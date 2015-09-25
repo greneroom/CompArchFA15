e5f1393c7158c9c8f0a95f11c3d2fae64e48c541
David Abrahams <david.abrahams@students.olin.edu>
2015-09-25 02:57:41 -0400

    simplified script

--- HW/HW2/hw2.sh
+++ HW/HW2/hw2.sh
@@ -4,6 +4,6 @@ iverilog adder.v -o add
 iverilog decoder.v -o decode
 iverilog multiplexer.v -o multi
 
-(echo 'Adder results'; ./add; echo; echo 'Decoder results'; ./decode; echo; echo 'Multiplexor results'; ./multi) > results.txt
+(./add; ./decode; ./multi) > results.txt
 
 rm add decode multi
